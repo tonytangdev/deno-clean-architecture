971af6ed2fd6395c85ee8c7d3362f51441ca3617
tonytangdev <tony.tang.developer@gmail.com>
2025-01-07 16:42:55 +0100

    Implement UserNotFound error and update DeleteUserUseCase to handle user non-existence

--- src/application/use-cases/delete-user.use-case.test.ts
+++ src/application/use-cases/delete-user.use-case.test.ts
@@ -2,12 +2,12 @@ import { beforeEach, describe, it, mock } from "node:test";
 import { UserRepository } from "../../domain/repositories/user.repository";
 import { DeleteUserUseCase } from "./delete-user.use-case";
 import assert from "node:assert";
-import { UserAlreadyExists } from "../errors/user-already-exists";
 import { UserRepositoryMock } from "./mocks/user.repository.mock";
+import { UserNotFound } from "../errors/user-not-found";
+import { User } from "../../domain/entities/user";
 
 describe("DeleteUserUseCase", () => {
   let userRepository: UserRepository;
-  let deleteByIdSpy = mock.fn();
 
   beforeEach(() => {
     mock.reset();
@@ -15,6 +15,10 @@ describe("DeleteUserUseCase", () => {
   });
 
   it("should throw an error if the user does not exist", async () => {
+    mock.method(UserRepositoryMock.prototype, "findById", () => {
+      return undefined;
+    });
+
     const userId = "123";
     const deleteUserUseCase = new DeleteUserUseCase(userRepository);
 
@@ -22,11 +26,20 @@ describe("DeleteUserUseCase", () => {
       await deleteUserUseCase.execute(userId);
       assert.fail("An error should have been thrown");
     } catch (error) {
-      assert.ok(error instanceof UserAlreadyExists);
-      assert.deepStrictEqual(error, new UserAlreadyExists());
+      assert.ok(error instanceof UserNotFound);
+      assert.deepStrictEqual(error, new UserNotFound());
     }
   });
   it("should delete a user", async () => {
+    const deleteByIdSpy = mock.fn((_: string) => {
+      return new User(
+        new Date(),
+        new Date(),
+        "email@email.com",
+        "username",
+        "123"
+      );
+    });
     mock.method(UserRepositoryMock.prototype, "findById", deleteByIdSpy);
 
     const userId = "123";
@@ -35,5 +48,24 @@ describe("DeleteUserUseCase", () => {
     await deleteUserUseCase.execute(userId);
     assert.ok(deleteByIdSpy.mock.calls.length === 1);
     assert.deepStrictEqual(deleteByIdSpy.mock.calls[0].arguments[0], userId);
+  });
+
+  it("throw an error when repository throws an error", async () => {
+    const expectedError = new Error("Unexpected error");
+    mock.method(UserRepositoryMock.prototype, "deleteById", () => {
+      throw expectedError;
+    });
+
+    const userId = "123";
+    const deleteUserUseCase = new DeleteUserUseCase(userRepository);
+
+    try {
+      await deleteUserUseCase.execute(userId);
+      assert.fail("An error should have been thrown");
+    } catch (error) {
+      console.log(error);
+      assert.ok(error instanceof Error);
+      assert.strictEqual(error, expectedError);
+    }
   });
 });
--- src/application/use-cases/delete-user.use-case.ts
+++ src/application/use-cases/delete-user.use-case.ts
@@ -1,13 +1,14 @@
 import { UserRepository } from "../../domain/repositories/user.repository";
 import { UserAlreadyExists } from "../errors/user-already-exists";
+import { UserNotFound } from "../errors/user-not-found";
 
 export class DeleteUserUseCase {
   constructor(private readonly userRepository: UserRepository) {}
 
   async execute(userId: string): Promise<void> {
     const user = await this.userRepository.findById(userId);
-    if (!!user) {
-      throw new UserAlreadyExists();
+    if (!user) {
+      throw new UserNotFound();
     }
 
     await this.userRepository.deleteById(userId);
